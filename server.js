d022f22608cef8be2ba9658a1b0ed293dba6fcae
latifa-git <latifabenameur96@gmail.com>
2021-07-04 12:23:19 +0100

    second commit

--- server.js
+++ server.js
@@ -1,34 +1,55 @@
 const express = require("express");
-require('dotenv').config({path: './config/.env'});
-const connectDB = require('./config/connectDB');
-const User = require('./models/User');
+require("dotenv").config({ path: "./config/.env" });
+const connectDB = require("./config/connectDB");
+const { findByIdAndUpdate } = require("./models/User");
+const User = require("./models/User");
 
 const app = express();
-
-const PORT = process.env.PORT || 5000;
-
-
-
 
 app.use(express.json());
 connectDB();
 
-//// CRUD ////
-// create read update delete
-// post put get delete
-
-// add users to the database
-app.post('/users/add', async (req, res) => {
-  const {name, email, phone} = req.body;
-  const newUser = new User({name, email, phone});
+app.post("/users/add", async (req, res) => {
+  const { name, email, phone } = req.body;
+  const newUser = new User({ name, email, phone });
   try {
     await newUser.save();
     res.send(newUser);
   } catch (error) {
-    res.send({error: error.message});
+    res.send({ error: error.message });
+  }
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, (err) =>
+  err ? console.error(err) : console.log(`server running on port ${PORT}`)
+);
+
+app.get("/users/get", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.send(error);
   }
 });
 
-app.listen(PORT, err =>
-  err ? console.error(err) : console.log(`server running on port ${PORT}`)
-);
+app.put("/users/update/:id", async (req, res) => {
+  try {
+    const editeduser = await User.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body },
+      { new: true }
+    );
+    res.send(editeduser);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.delete("/users/delete/:id", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.send({ msg: "done" });
+  } catch (error) {}
+});
+
